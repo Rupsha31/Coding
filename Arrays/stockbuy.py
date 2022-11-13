11abbac070baf07c76c9da1359937ff0e49fca64
Rupsha Nandy <nmoon2795@gmail.com>
2022-11-13 16:32:02 +0530

    initial commit

---
+++ Arrays/stockbuy.py
@@ -0,0 +1,32 @@
+def stockBuySell(A,n):
+    l=[]
+    for j in range(n-1):
+        if A[j] < A[j+1]:
+            l.append((j,j+1))
+    return l
+
+def check(ans,A,p):
+    c = 0
+    for i in range(len(ans)):
+        c += A[ans[i][1]]-A[ans[i][0]]
+    if(c==p):
+        return 1 
+    else:
+        return 0
+
+if __name__=='__main__':
+	t = int(input())
+	while(t>0):
+		n = int(input())
+		A = [int(x) for x in input().strip().split()]
+		ans =stockBuySell(A,n)
+		p=0
+		for i in range(n-1):
+		    p += max(0,A[i+1]-A[i])
+		if(len(ans) == 0):
+			print("No Profit",end="")
+		else:
+			print(check(ans,A,p),end="")
+		print()
+		t-=1
+# } Driver Code Ends
